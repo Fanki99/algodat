9ee89677daf8dffb74042fbe2e76d993a91698a3
Fanki99 <thomas.fankhauser1@gmail.com>
2019-05-06 13:49:13 +0200

    add initial project files
    Signed-off-by: Fanki99 <thomas.fankhauser1@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ALGODAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------");
+            Console.WriteLine("Willkommen bei dem Aktien-Programm");
+            Console.WriteLine("von Thomas & Thomas");
+            Console.WriteLine("----------------");
+        }
+    }
+}
