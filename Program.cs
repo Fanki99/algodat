599013766d12b239b8173c732bd1e30c51ecb5e5
Fanki99 <thomas.fankhauser1@gmail.com>
2019-06-09 12:37:31 +0200

    first setup ueb4
    Signed-off-by: Fanki99 <thomas.fankhauser1@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace algodat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
